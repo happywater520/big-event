a1d50632ee19ea2b9571a16c33541e91eb92d526
happywater520 <3492565885@qq.com>
2023-04-18 11:48:38 +0800

    第一次提交

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,25 @@
+
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+  <div>
+    <el-button>默认按钮</el-button>
   </div>
 </template>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+import { getListAPI } from '@/api'
+export default {
+  created () {
+    this.getListFn()
+  },
+  methods: {
+    async getListFn () {
+      const res = await getListAPI()
+      console.log(res)
     }
   }
 }
+
+</script>
+
+<style lang="less" scoped>
 </style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import '@/assets/global.less' // 全局初始化样式
+// import ElementUI from 'element-ui'
+// import 'element-ui/lib/theme-chalk/index.css'
+
+// Vue.use(ElementUI)
+import '@/elementUI' // 注册elementUI组件
 
 Vue.config.productionTip = false
 
